a54b26f60ad36b75d345f7b8e14b282b4281a3b6
Aadil Mugal <64457787+aadilmughal786@users.noreply.github.com>
2021-07-19 22:05:08 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,23 @@
+const name = prompt("🎷🎷🎷🎷\nEnter your name");
+alert(`Nice to meet you ${name}`);
+var card = 5;
+while(true){
+	let guess = prompt(`${card} 🃏  Cards left\n💡Can you guess my name : `);
+
+	let name = "Aadil";
+
+	if(guess==name){
+		alert("you are too genius🎉🎉🎉🎉🎉🎉");
+		break;
+	}
+	else{
+		alert("you are wrong👾👾👾👾👾👾");
+		card--;
+		if(card==0){
+			alert("You are not a good guesser 👻👻👻👻👻")
+			break;
+		}
+	}
+
+}
+
